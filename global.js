441a4ab52f600c87e1aa5cae4fbafcb0a048b449
giahuyyphung <169174586+giahuyyphung@users.noreply.github.com>
2024-06-05 21:21:19 +0700

    Update global.js

--- global.js
+++ global.js
@@ -9,7 +9,7 @@ const globalChannels = [
 const staffIds = ["1055695302386012212", "1242330820677603359", "948220309176221707"]; // Replace with actual staff member IDs
 const botStaffIds = ["1112683447366991923", "1236505346814644326"]; // Replace with actual staff bot IDs
 const partneredServerIds = ["1090877567210356768","1220232261228564601", "1003698094187216898"]; // Replace with actual partnered server IDs
-const ownerIds = ["1015763488938938388"]; // Replace with actual owner IDs
+const ownerIds = ["1015763488938938388", "1157629753742856222"]; // Replace with actual owner IDs
 
 module.exports = client => {
     // First some supportive buttons!
@@ -55,6 +55,9 @@ module.exports = client => {
 
             // Add icons for staff, bot staff, and partnered servers
             if (staffIds.includes(message.author.id)) {
+                embed.setDescription(`<a:hc_Moderator:1094886268942422097> ${embed.description}`);
+            }
+            if (ownerIds.includes(message.author.id)) {
                 embed.setDescription(`<a:hg_king:1080873872578064444> ${embed.description}`);
             }
 
