796d4e6f769151087ff894fc29e143e6584d315f
giahuyyphung <169174586+giahuyyphung@users.noreply.github.com>
2024-06-05 19:39:12 +0700

    Update global.js

--- global.js
+++ global.js
@@ -8,7 +8,7 @@ const globalChannels = [
 
 const staffIds = ["1015763488938938388", "1055695302386012212", "1242330820677603359", "948220309176221707"]; // Replace with actual staff member IDs
 const botStaffIds = ["1112683447366991923", "1236505346814644326"]; // Replace with actual staff bot IDs
-const partneredServerIds = ["1090877567210356768","1220232261228564601", "1247891607341236234"]; // Replace with actual partnered server IDs
+const partneredServerIds = ["1090877567210356768","1220232261228564601", "1003698094187216898"]; // Replace with actual partnered server IDs
 
 module.exports = client => {
     // First some supportive buttons!
